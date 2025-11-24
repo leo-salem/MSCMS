1fb31f580042fd646862af3f49807f37fa269758
tefaa1 <mh8369629@gmail.com>
2025-11-24 22:58:42 +0200

    tefa

--- src/main/java/com/example/Multi_sport_Club_Management_system/MultiSportClubManagementSystemApplication.java
+++ src/main/java/com/example/Multi_sport_Club_Management_system/MultiSportClubManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MultiSportClubManagementSystemApplication {
 
 	/*welcome by el saloooom */
+	/*welcome by el tefaaaaa */
 	public static void main(String[] args) {
 		SpringApplication.run(MultiSportClubManagementSystemApplication.class, args);
 	}
